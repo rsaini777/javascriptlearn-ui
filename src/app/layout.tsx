5ff08db74314c205022a551e4f6993fe9ca2df33
rsainiv3 <rahul@v3cars.com>
2025-08-29 21:26:46 +0530

    adsence

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,35 +18,36 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "JavaScriptLearning",
-  description: "Practice frontend interview questions and learn from detailed solutions.",
+  description:
+    "Practice frontend interview questions and learn from detailed solutions.",
   icons: {
     icon: [
-      { url: '/favicon.ico' },
-      { url: '/icon.png', type: 'image/png', sizes: '32x32' },
+      { url: "/favicon.ico" },
+      { url: "/icon.png", type: "image/png", sizes: "32x32" },
     ],
-    apple: [
-      { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
+    apple: [{ url: "/apple-icon.png", sizes: "180x180", type: "image/png" }],
   },
-  manifest: '/manifest.json',
+  manifest: "/manifest.json",
   openGraph: {
-    title: 'JavaScriptLearning',
-    description: 'Practice frontend interview questions and learn from detailed solutions.',
-    type: 'website',
+    title: "JavaScriptLearning",
+    description:
+      "Practice frontend interview questions and learn from detailed solutions.",
+    type: "website",
     images: [
       {
-        url: '/Images/logo.png',
+        url: "/Images/logo.png",
         width: 1200,
         height: 630,
-        alt: 'JavaScriptLearning Logo',
+        alt: "JavaScriptLearning Logo",
       },
     ],
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'JavaScriptLearning',
-    description: 'Practice frontend interview questions and learn from detailed solutions.',
-    images: ['/Images/logo.png'],
+    card: "summary_large_image",
+    title: "JavaScriptLearning",
+    description:
+      "Practice frontend interview questions and learn from detailed solutions.",
+    images: ["/Images/logo.png"],
   },
 };
 
@@ -57,17 +58,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-1749438252884284"></meta>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID|| 'G-0HD1V0CS5Z'} />
+          <GoogleAnalytics
+            GA_TRACKING_ID={
+              process.env.NEXT_PUBLIC_GA_TRACKING_ID || "G-0HD1V0CS5Z"
+            }
+          />
+       
           <Navbar />
-          <main className="min-h-screen">
-            {children}
-            
-
-          </main>
+          <main className="min-h-screen">{children}</main>
           <Footer />
         </AuthProvider>
       </body>
